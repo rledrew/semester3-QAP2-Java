308ef90d1ebf2614f901773758485812375eaabe
Riley <riley.ledrew@keyin.com>
2023-06-06 15:07:46 -0230

    LN 24 CCDemo was charge when it should be payment

--- Money.java
+++ Money.java
@@ -24,14 +24,12 @@ public class Money {
 
     public Money subtract(Money other) {
         Money difference = new Money(0);
-    
-        long thisTotalCents = this.dollars * 100 + this.cents;
-        long otherTotalCents = other.dollars * 100 + other.cents;
-
-        long differenceTotalCents = thisTotalCents - otherTotalCents;
-    
-        difference.dollars = differenceTotalCents / 100;
-        difference.cents = differenceTotalCents % 100;
+        difference.cents = this.cents - other.cents;
+        difference.dollars = this.dollars - other.dollars;
+        if (difference.cents < 0) {
+            difference.cents = 100 - Math.abs(difference.cents);
+            difference.dollars--;
+        }
     
         return difference;
     }
